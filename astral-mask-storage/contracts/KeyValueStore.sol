1a6a69c15b6095a05802d4a119a00ec728b8534c
nikaidou-haruki <meteo.haru@gmail.com>
2023-12-20 14:02:43 +0000

    add metaStore Contract

--- astral-mask-storage/contracts/KeyValueStore.sol
+++ astral-mask-storage/contracts/KeyValueStore.sol
@@ -4,12 +4,16 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+interface MetaStore {
+    function addMeta(uint256 tokenId, string calldata _sheetName) external;
+}
+
 contract KeyValueStore is ERC721URIStorage, Ownable {
     // Each ERC721 token is a key to access the k-v sheet
     mapping(uint256 => mapping(string => string)) private keyValueStore;
 
     uint256 private tokenIdCounter;
-
+    address private metaStoreAddress;
     string private baseTokenURI;
 
     // set `baseURI` (only owner)
@@ -24,12 +28,15 @@ contract KeyValueStore is ERC721URIStorage, Ownable {
         updateBaseURI(_baseTokenURI);
     }
 
-    // Set MetaId when mint
-    function newSheet(string memory metaId) public {
+    // create a sheet and mint a ERC720 token
+    function newSheet(string memory sheetName) public returns (uint256) {
         uint256 newTokenId = tokenIdCounter;
         _safeMint(msg.sender, newTokenId);
-        _setTokenURI(newTokenId, string(abi.encodePacked(baseTokenURI, metaId)));
+        _setTokenURI(newTokenId, string(abi.encodePacked(baseTokenURI, newTokenId)));
+        MetaStore metaStore = MetaStore(metaStoreAddress);
+        metaStore.addMeta(newTokenId, sheetName);
         tokenIdCounter++;
+        return tokenIdCounter - 1;
     }
 
     function setKeyValue(uint256 tokenId, string memory key, string memory value) public {
---
+++ astral-mask-storage/contracts/metaStore.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+contract MetaStorage {
+    string private namePrefix;
+    string private description;
+    string private image;
+    mapping(uint256 => string) private sheetNames;
+    address private keyValueStore;
+
+    address private owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    modifier onlyKvStore() {
+        require(msg.sender == keyValueStore, "only KeyValueStore Conrtact Can Call this Function");
+        _;
+    }
+
+    function setNamePrefix(string calldata _namePrefix) external onlyOwner {
+        namePrefix = _namePrefix;
+    }
+
+    function setDescription(string calldata _description) external onlyOwner {
+        description = _description;
+    }
+
+    function setKvStoreAddress(address _kvAddress) external onlyOwner {
+        keyValueStore = _kvAddress;
+    }
+
+    function setImage(string calldata _image) external onlyOwner {
+        image = _image;
+    }
+
+    function addSheetName(uint256 tokenId, string calldata _sheetName) external onlyKvStore {
+        sheetNames[tokenId] = _sheetName;
+    }
+
+    function getTokenMeta(uint256 tokenId) external view returns (string memory) {
+        string memory tokenName = string(abi.encodePacked(namePrefix, " #", uintToString(tokenId)));
+        string memory json = string(abi.encodePacked(
+            '{"name":"', tokenName, 
+            '", "image":"', image,
+            '", "description":"', description,
+            '", "attributes": { "trait_type": "Sheet Name", "value": "', sheetNames[tokenId], '"}}'
+        ));
+        return json;
+    }
+
+    function addMeta(uint256 tokenId, string calldata _sheetName) external onlyOwner {
+        sheetNames[tokenId] = _sheetName;
+    }
+
+    function uintToString(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+}
